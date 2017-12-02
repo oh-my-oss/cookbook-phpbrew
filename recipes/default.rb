66a84dc386f2c5961bc75da74a2b25e5951ade3c
Shinichi Urabe <s-urabe@oz-vision.co.jp>
2017-12-02 21:53:39 +0900

    fix 'checking for cURL 7.10.5 or greater... configure: error: cURL version 7.10.5 or later is required to compile php with cURL support'

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ end
     package pkg
 end
 
-%w(libxml2-devel openssl-devel bzip2-devel).each do |pkg|
+%w(libxml2-devel openssl-devel bzip2-devel libcurl-devel).each do |pkg|
     package pkg
 end
 
