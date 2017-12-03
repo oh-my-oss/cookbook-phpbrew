bbb26a2f0374111ef1f345c43ec22bb68d556267
Shinichi Urabe <s-urabe@oz-vision.co.jp>
2017-12-03 22:37:29 +0900

    update bashrc and set PHPBREW_ROOT to /usr/local/lib64/phpbrew path.

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,12 +12,18 @@ execute 'install phpbrew' do
 end
 
 execute 'setup bashrc' do
-    command "echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc && export PATH=$PATH:/usr/local/bin"
+    command "echo 'export PATH=$PATH:/usr/local/bin' >> /etc/bashrc"
+    not_if "grep 'export PATH=$PATH:/usr/local/bin' /etc/bashrc"
 end
 
-execute 'phpbrew init' do
-    command "/usr/local/bin/phpbrew init && echo '[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc' >> ~/.bashrc && . ~/.bashrc && /usr/local/bin/phpbrew known"
-    not_if { Dir.exists?('~/.phpbrew') }
+execute 'setup bashrc2' do
+    command "echo 'export PHPBREW_ROOT=/usr/local/lib64/phpbrew' >> /etc/bashrc"
+    not_if "grep 'export PHPBREW_ROOT=/usr/local/lib64/phpbrew' /etc/bashrc"
+end
+
+execute 'setup bashrc3' do
+    command "echo '[[ -e /usr/local/lib64/phpbrew/bashrc ]] && source /usr/local/lib64/phpbrew/bashrc' >> /etc/bashrc"
+    not_if "grep '[[ -e /usr/local/lib64/phpbrew/bashrc ]] && source /usr/local/lib64/phpbrew/bashrc' /etc/bashrc"
 end
 
 execute 'phpbrew init' do
