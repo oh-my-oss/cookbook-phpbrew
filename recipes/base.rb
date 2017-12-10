7f7919d9ba8978aecbcc41476204e32c6990978c
Shinichi Urabe <s-urabe@oz-vision.co.jp>
2017-12-11 00:52:50 +0900

    Install php70 and set date.timezone

--- recipes/base.rb
+++ recipes/base.rb
@@ -2,9 +2,19 @@
     package pkg
 end
 
+%w(php70 php70-common php70-devel).each do |pkg|
+    package pkg
+end
+
 directory "/usr/local/lib64/phpbrew" do
     owner "root"
     group "root"
     mode 0755
     action :create
+end
+
+template '/etc/php-7.0.d/99-timezone.ini' do
+    owner 'root'
+    mode 0755
+    source '99-timezone.ini.erb'
 end
